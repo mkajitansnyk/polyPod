7a01e812b76319900e7d8e9564f3c14d31880ff2
irstavr <irini.stavrakantonaki@polypoly.net>
2022-05-13 11:37:45 +0200

    add new endpoint for error reports to polyApi backend

--- platform/utils/endpoints-generator/endpoints.js
+++ platform/utils/endpoints-generator/endpoints.js
@@ -3,6 +3,10 @@ export default {
         url: process.env.POLYPOD_POLYPEDIA_REPORT_URL,
         auth: process.env.POLYPOD_POLYPEDIA_REPORT_AUTHORIZATION || "",
     },
+    polyApiErrorReport: {
+        url: process.env.POLYPOD_ERROR_REPORT_URL,
+        auth: process.env.POLYPOD_ERROR_REPORT_AUTHORIZATION || "",
+    },
     demoTest: {
         url: "",
         allowInsecure: true,
